2ba12df7151d9a14c74aaaace5b48b7e97f11051
olga.kuriatnyk <olha.kuriatnyk@gmail.com>
2023-04-12 23:39:36 -0700

    show cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
 
+import { useCallback, useEffect, useState } from 'react';
+import Card from './Components/Card';
+import { getData } from './fetch';
+import './style.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
-  return null;
-}
+
+  const [items, setItems] = useState([]);
+
+  const handleGetData = useCallback(() => {
+    getData().then(data => {
+      setItems(data);
+    })
+  }, []);
+
+  useEffect(() => {
+    handleGetData();
+  }, [handleGetData]);
+
+  function handleClick() {
+    getData().then(data => {
+      setItems(data);
+    })
+  }
+
+  return (
+    <div>
+      <button type='button' className='btn btn-outline-primary' onClick={handleClick}>Refresh Compounds</button>
+      <div class="row row-cols-1 row-cols-md-3 g-4">
+        {items.map((compound) => {
+          return (
+              <Card
+                title={compound.title}
+                subtitle={compound.subtitle}
+                description={compound.description}
+                image={compound.image}
+              />
+          )
+        })}
+      </div>
+    </div>
+
+  );
+};
 
 export default App;
+
