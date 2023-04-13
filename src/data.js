458ecf1aa4050fcdbe8d2c25a6695575c8a63d19
olga.kuriatnyk <olha.kuriatnyk@gmail.com>
2023-04-12 23:39:47 -0700

    add styles

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const compounds = [
+    {
+        title: "Chromate",
+        subtitle: "CID: 24461",
+        description: "Chromate(2-) is a chromium oxoanion resulting from the removal of two protons from chromic acid. It has a role as an oxidising agent. It is a divalent inorganic anion and a chromium oxoanion. It is a conjugate base of a hydrogenchromate.",
+        imageURL: "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/chromate/PNG",
+    },
+    {
+        title: "Water",
+        subtitle: "CID: 962",
+        description: "Water is an oxygen hydride consisting of an oxygen atom that is covalently bonded to two hydrogen atoms It has a role as an amphiprotic solvent, a member of greenhouse gas, a human metabolite, a Saccharomyces cerevisiae metabolite, an Escherichia coli metabolite and a mouse metabolite. It is an oxygen hydride, a mononuclear parent hydride and an inorganic hydroxy compound. It is a conjugate base of an oxonium. It is a conjugate acid of a hydroxide.",
+        imageURL: "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/water/PNG",
+    },
+    {
+        title: "Methanol",
+        subtitle: "CID: 887",
+        description: "Methanol is the primary alcohol that is the simplest aliphatic alcohol, comprising a methyl and an alcohol group. It has a role as an amphiprotic solvent, a fuel, a human metabolite, an Escherichia coli metabolite, a mouse metabolite and a Mycoplasma genitalium metabolite. It is an alkyl alcohol, a one-carbon compound, a volatile organic compound and a primary alcohol. It is a conjugate acid of a methoxide.",
+        imageURL: "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/methanol/PNG",
+    },
+    {
+        title: "Caffeine",
+        subtitle: "CID: 2519",
+        description: "Caffeine is a trimethylxanthine in which the three methyl groups are located at positions 1, 3, and 7. A purine alkaloid that occurs naturally in tea and coffee. It has a role as a central nervous system stimulant, an EC 3.1.4.* (phosphoric diester hydrolase) inhibitor, an adenosine receptor antagonist, an EC 2.7.11.1 (non-specific serine/threonine protein kinase) inhibitor, a ryanodine receptor agonist, a fungal metabolite, an adenosine A2A receptor antagonist, a psychotropic drug, a diuretic, a food additive, an adjuvant, a plant metabolite, an environmental contaminant, a xenobiotic, a human blood serum metabolite, a mouse metabolite, a geroprotector and a mutagen. It is a purine alkaloid and a trimethylxanthine.",
+        imageURL: "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/caffeine/PNG",
+    },
+    {
+        title: "Deae-cellulose",
+        subtitle: "CID: 16211032",
+        description: "DEAE-cellulose is a glycoside.",
+        imageURL: "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/cellulose/PNG",
+    }
+];
+
+export default compounds;
