525b8f2ed475e933fef6a77508b80cae4a339e36
olga.kuriatnyk <olha.kuriatnyk@gmail.com>
2023-04-14 10:12:03 -0700

    fix TypeError and add key

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-
-import { useCallback, useEffect, useState } from 'react';
-import Card from './Components/Card';
-import { getData } from './fetchData';
-import './styles.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useCallback, useEffect, useState } from "react";
+import Card from "./Components/Card";
+import { getData } from "./fetchData";
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
   const [items, setItems] = useState([]);
 
   const handleGetData = useCallback(() => {
-    getData().then(data => {
+    getData().then((data) => {
       setItems(data);
-    })
+    });
   }, []);
 
   useEffect(() => {
@@ -20,32 +18,37 @@ function App() {
   }, [handleGetData]);
 
   function handleClick() {
-    getData().then(data => {
+    getData().then((data) => {
       setItems(data);
-    })
+    });
   }
 
   return (
     <div>
-      <div className='container'>
-        <button type='button' className='btn btn-outline-primary' onClick={handleClick}>Refresh Compounds</button>
+      <div className="container">
+        <button
+          type="button"
+          className="btn btn-outline-primary"
+          onClick={handleClick}
+        >
+          Refresh Compounds
+        </button>
       </div>
-      <div class='row row-cols-1 row-cols-md-3 g-4'>
-        {items.map((compound) => {
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {items.map(function (compound, index) {
           return (
             <Card
+              key={index}
               title={compound.title}
               subtitle={compound.subtitle}
               description={compound.description}
               image={compound.image}
             />
-          )
+          );
         })}
       </div>
     </div>
-
   );
-};
+}
 
 export default App;
-
--- src/fetchData.js
+++ src/fetchData.js
@@ -1,75 +1,144 @@
-const allCompounds = ['water', 'chromate', 'methanol', 'caffeine', 'cellulose',
-    'ethanol', 'sulfuric-acid', 'acetic-acid', 'hydrochloric-acid', 'acetate',
-    'ammonia', 'nitric-acid', 'phosphoric-acid', 'calcium-carbonate', 'ammonium-sulfate',
-    'carbonic-acid', 'sodium-bicarbonate', 'sodium-hydroxide', 'calcium-hydroxide', 'sucrose',
-    'sodium-carbonate', 'phenol', 'nitrous-acid', 'potassium-hydroxide', 'silver-nitrate',
-    'sodium-chloride', 'sulfurous-acid', 'magnesium-hydroxide', 'methane', 'nitrogen-dioxide',
-   'aluminum-oxide', 'ammonium-nitrate', 'ammonium-phosphate', 'barium-hydroxide', 'iron-oxide',
-    'carbon-tetrachloride', 'citric-acid', 'salicylic-acid', 'potassium-carbonate', 'silver-chloride',
-    'calcium-acetate', 'sodium-iodide', 'sodium-oxide', 'sodium-sulfide'];
-
-// const compounds = getRandomCompounds(5);
-
-function getDescriptions(compounds) {
+const allCompounds = [
+    "water",
+    "methanol",
+    "caffeine",
+    "cellulose",
+    "ethanol",
+    "sulfuric-acid",
+    "acetic-acid",
+    "hydrochloric-acid",
+    "acetate",
+    "ammonia",
+    "nitric-acid",
+    "phosphoric-acid",
+    "calcium-carbonate",
+    "ammonium-sulfate",
+    "carbonic-acid",
+    "sodium-bicarbonate",
+    "calcium-hydroxide",
+    "sucrose",
+    "sodium-carbonate",
+    "phenol",
+    "nitrous-acid",
+    "potassium-hydroxide",
+    "silver-nitrate",
+    "sulfurous-acid",
+    "magnesium-hydroxide",
+    "methane",
+    "nitrogen-dioxide",
+    "aluminum-oxide",
+    "ammonium-nitrate",
+    "barium-hydroxide",
+    "iron-oxide",
+    "carbon-tetrachloride",
+    "citric-acid",
+    "salicylic-acid",
+    "potassium-carbonate",
+    "silver-chloride",
+    "calcium-acetate",
+    "sodium-iodide",
+    "sodium-oxide",
+    "sodium-sulfide"
+  ];
+  
+  // this fuction returns an array with data (compounds name, CID, description, etc)
+  function getDescriptionsBatch(compounds) {
     return new Promise(function (resolve, reject) {
-        Promise.all(compounds.map(name =>
-            fetch(`https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/${name}/description/JSON`)
-                .then(res => res.json())))
-            .then((data) => {
-                resolve(data);
-            })
-            .catch((err) => {
-                reject(err);
-            });
+      // get a promis for each compound, store in array
+      let descrPromises = compounds.map((name) => getDescription(name));
+      // if successful return array with data
+      Promise.all(descrPromises)
+        .then((data) => {
+          resolve(data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
     });
-}
-
-function getImages(compounds) {
+  }
+  
+  // make an GET request, to get data for the componound
+  function getDescription(compound) {
     return new Promise(function (resolve, reject) {
-        Promise.all(compounds.map(name =>
-            fetch(`https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/${name}/PNG`)
-        ))
-            .then((data) => {
-                resolve(data);
-            })
-            .catch((err) => {
-                reject(err);
-            });
+      fetch(
+        `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/${compound}/description/JSON`
+      )
+        .then((res) => resolve(res.json()))
+        .catch((err) => {
+          reject(err);
+        });
     });
-}
-
-function getRandomCompounds(num) {
+  }
+  
+  // this function returns arrys with images
+  function getImagesBatch(compounds) {
+    return new Promise(function (resolve, reject) {
+      let imgPromises = compounds.map((name) => getImage(name));
+      Promise.all(imgPromises)
+        .then((data) => {
+          resolve(data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+  
+  // make an GET request, to get image for the componound
+  function getImage(compound) {
+    return new Promise(function (resolve, reject) {
+      fetch(
+        `https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/${compound}/PNG`
+      )
+        .then((res) => resolve(res))
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+  
+  function getRandomCompounds(num) {
     const shuffled = [...allCompounds].sort(() => 0.5 - Math.random());
     return shuffled.slice(0, num);
-}
-
-export function getData() {
-    console.log("getdata");
+  }
+  
+  // call functions to get data from the API for 5 randomly chosen compounds,
+  // then processes recived data, merging into array of compounds
+  // return array of 5 compounds including title, subtitle (CID), description and image
+  export function getData() {
     const compounds = getRandomCompounds(5);
     return new Promise(function (resolve, reject) {
-
-        let descriptionsPromise = getDescriptions(compounds);
-        let imagesPromise = getImages(compounds);
-
-        Promise.all([descriptionsPromise, imagesPromise]).then(data => {
-            let descriptions = data[0];
-            let images = data[1];
-            var compoundsData = [];
-            //merge data
-            for (let i = 0; i < compounds.length; i++) {
-                const comp = {
-                    title: descriptions[i].InformationList.Information[0].Title,
-                    subtitle: descriptions[i].InformationList.Information[0].CID,
-                    description: descriptions[i].InformationList.Information[1].Description,
-                    image: images[i].url
-                }
-                compoundsData.push(comp);
-            }
-            resolve(compoundsData);
+      let descriptionsPromise = getDescriptionsBatch(compounds);
+      let imagesPromise = getImagesBatch(compounds);
+  
+      Promise.all([descriptionsPromise, imagesPromise])
+        .then((data) => {
+          let descriptions = data[0];
+          let images = data[1];
+          var compoundsData = [];
+  
+          //merge data
+          for (let i = 0; i < compounds.length; i++) {
+            let titleData =
+              descriptions[i].InformationList?.Information[0]?.Title;
+            let subtitleData =
+              descriptions[i].InformationList?.Information[0]?.CID;
+            let descriptionData =
+              descriptions[i].InformationList?.Information[1]?.Description;
+            let imageData = images[i]?.url;
+            const comp = {
+              title: titleData,
+              subtitle: subtitleData,
+              description: descriptionData,
+              image: imageData
+            };
+            compoundsData.push(comp);
+          }
+          resolve(compoundsData);
         })
-            .catch(error => {
-                reject(error);
-
-            })
+        .catch((error) => {
+          reject(error);
+        });
     });
-}
+  }
+  
